d85e9f43a1057d21c64a857c3355a3113899a12f
unknown <zaza909077777@gmail.com>
2022-12-03 22:37:21 +0200

    2 update

---
+++ js/plagen.js
@@ -0,0 +1,24 @@
+///////////////////////////////////////// owl-carousel ////////////////////////////////
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    margin: 10,
+    nav: false,
+    mouseDrag: true,
+    dotsEach: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      798: {
+        items: 1,
+      },
+    },
+  });
+});
+///////////////////////////////////////// owl-carousel ////////////////////////////////
